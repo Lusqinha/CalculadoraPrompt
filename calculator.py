ec41914ff5266f1b792ba4701d7af38043cb4f46
Lucas Borges <86315861+Lusqinha@users.noreply.github.com>
2021-10-16 01:52:02 -0300

    Correção de erros e adição de funcionalidades 🛸
    Correção de um erro grotesco no código, modificação no código do calculo da Hipotenusa, adição da funcionalidade para calcular Seno, Cosseno e Tangente.🚊

--- calculator.py
+++ calculator.py
@@ -1,6 +1,7 @@
 from rich.table import Table
 from rich.console import Console
 from rich import print
+from math import sin, cos, tan, radians, hypot
 
 
 # bhaskara
@@ -18,25 +19,18 @@ def bhaskara():
         print(f'>delta é {delta}\n >X¹ é {x1:.2f} \n >X² é {x2:.2f}')
 
 
-# pitagoras
-def pitagoras():
-    var1 = input('Qual valor deseja descobrir?\n >H  Hipotenusa\n >CA Cateto Adjacente\n >CO Cateto Oposto\n >>>')
-    if var1 == 'H' or var1 == 'h':
-        ca = float(input(f'Qual o valor do Cateto Adjacente?\n >'))
-        co = float(input(f'Qual o valor do Cateto Oposto?\n >'))
-        hipotenusa = (ca ** 2 + co ** 2) ** (1 / 2)
-        print(f'O valor da hipotenusa é {hipotenusa:.2f}')
-    elif var1 == 'CA' or var1 == 'ca':
-        print('Função em desenvolvimento! | Perdão pelo transtorno ')
-        pass
-    elif var1 == 'CO' or var1 == 'co':
-        print('Função em desenvolvimento! | Perdão pelo transtorno ')
-        pass
+# Hipotenusa
+def hipotenusa():
+    ca, co = float(input("Insira o valor do Cateto Adjacente e do Cateto Oposto respectivamente.\n >>>"))
+    try:
+        Hipotenusa = hypot(ca, co)
+    except (ValueError, ZeroDivisionError, SyntaxError):
+        print("Não foi possível obter qualquer resultado.")
     else:
-        print('Escolha uma opção válida!!! | Você está sendo encaminhado para a tela inicial. ')
-        pass
+        print(f'A hipotenusa de {co}(Co) e {ca}(Ca) é {Hipotenusa}')
 
 
+# 2° lei de Newton: Valor de A
 def calculoA():
     F = float(input(f'insira o valor de F \n >>>'))
     M = float(input(f'Insira o valor de M \n >>>'))
@@ -44,6 +38,7 @@ def calculoA():
     print(f'O valor da Aceleração é {A}m/s².')
 
 
+# 2° lei de Newton: Valor de F
 def calculoF():
     A1 = float(input(f'insira o valor de A \n >>>'))
     M1 = float(input(f'Insira o valor de M \n >>>'))
@@ -51,6 +46,7 @@ def calculoF():
     print(f'O Valor da Força é {F1}N.')
 
 
+# 2° lei de Newton: Valor de M
 def calculoM():
     F2 = float(input(f'insira o valor de F \n >>>'))
     A2 = float(input(f'insira o valor de A \n >>>'))
@@ -58,30 +54,35 @@ def calculoM():
     print(f'O valor de massa é [green]{M2}[/]Kg.')
 
 
+# Operações básicas: Multiplicação
 def multiplicacao():
     M1 = float(input(f'Insira o 1° valor\n >>>'))
     M2 = float(input(f'Insira o 2° valor\n >>>'))
     print(M1 * M2)
 
 
+# Operações básicas: Soma
 def soma():
     S1 = float(input(f'Insira o 1° valor\n >>>'))
     S2 = float(input(f'Insira o 2° valor\n >>>'))
     print(S1 + S2)
 
 
+# Operações básicas: Subtração
 def subtracao():
     Su1 = float(input(f'Insira o 1° valor\n >>>'))
     Su2 = float(input(f'Insira o 2° valor\n >>>'))
     print(Su1 - Su2)
 
 
+# Operações básicas: Divisão
 def divisao():
     D1 = float(input(f'Insira o 1° valor\n >>>'))
     D2 = float(input(f'Insira o 2° valor\n >>>'))
     print(D1 // D2)
 
 
+# Operações básicas: menu
 def operacoesbasicas():
     ob_table = Table(title='Qual operação você deseja realizar?', title_style='purple')
     ob_table.add_column('Soma', justify='center')
@@ -101,23 +102,64 @@ def operacoesbasicas():
         divisao()
 
 
+# Seno
+def sencostan():
+    an = float(input(f'Insira o valor do ângulo.\n >>>'))
+    console1 = Console()
+    table2 = Table(title="Seno, Cosseno ou Tangente?", title_justify='center')
+    table2.add_column('Seno', justify='center')
+    table2.add_column('Cosseno', justify='center')
+    table2.add_column('Tangente', justify='center')
+    table2.add_row('sen', 'cos', 'tan')
+    console1.print(table2)
+    es = str(input('>>>'))
+
+    if es == 'sen' or es == 'Sen':
+        try:
+            Vr1 = sin(radians(an))
+        except (ZeroDivisionError, ValueError, SyntaxError):
+            print("Não foi possível obter qualquer resultado.")
+        else:
+            print(f"O Seno de {an} é {Vr1:.4f}")
+    elif es == 'cos' or es == 'Cos':
+        try:
+            Vr2 = cos(radians(an))
+        except(ZeroDivisionError, ValueError, SyntaxError):
+            print("Não foi possível obter qualquer resultado.")
+        else:
+            print(f"O Cosseno de {an} é {Vr2:.4f}")
+    elif es == 'tan' or es == 'Tan':
+        try:
+            Vr3 = tan(radians(an))
+        except (ZeroDivisionError, ValueError, SyntaxError):
+            print("Não foi possível obter qualquer resultado.")
+        else:
+            print(f"A Tangente de {an} é {Vr3:.4f}")
+    else:
+        print("Opção inválida!")
+        pass
+
+
 # Loop Principal
 calcular = True
 while calcular:
+    # Criando Interface no Prompt de comando
     console = Console()
     table = Table(title="Calculadora em Python", title_justify='center')
     table.add_column('Bhaskara', justify='center')
-    table.add_column('Teorema de Pitágoras', justify='center')
-    table.add_column('Aceleração da Gravidade', justify='center')
+    table.add_column('Hipotenusa', justify='center')
+    table.add_column('Segunda Lei de Newton', justify='center')
     table.add_column('Operações básicas', justify='center')
+    table.add_column('Seno, Cosseno e Tangente', justify='center')
     table.add_column('Fechar', justify='center', style='red')
-    table.add_row('1', '2', '3', '4', 'X')
+    table.add_row('1', '2', '3', '4', '5', '0')
     console.print(table)
+    # Opções da calculadora
     escolha = int(input('>>>'))
     if escolha == 1:
         bhaskara()
     elif escolha == 2:
-        pitagoras()
+        hipotenusa()
     elif escolha == 3:
         table1 = Table(title="F = M . A")
         table1.add_column('Força', justify='center')
@@ -125,7 +167,7 @@ while calcular:
         table1.add_column('Aceleração', justify='center')
         table1.add_row('1', '2', '3')
         console.print(table1)
-        resposta = input(f'Qual é a icognita? 1, 2, 3? \n >>>')
+        resposta = input(f'Qual é a icognita?\n >>>')
 
         if resposta == '1':
             calculoF()
@@ -140,7 +182,9 @@ while calcular:
             pass
     elif escolha == 4:
         operacoesbasicas()
-    elif escolha == 'x' or escolha == 'X':
+    elif escolha == 5:
+        sencostan()
+    elif escolha == 0:
         calcular = False
     else:
-        print('escolha uma opção válida!!!'.format())
+        print('escolha uma opção válida!!!')
